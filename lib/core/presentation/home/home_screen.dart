86f75706115768d2cb32faa8e32e4c1a749e5d0e
Jad Abu Awwad <jad.abuawwad@optimumpartners.com>
2023-02-27 16:17:58 +0300

    fix : platform targeted and ignore some files

--- lib/core/presentation/home/home_screen.dart
+++ lib/core/presentation/home/home_screen.dart
@@ -1,11 +1,11 @@
-import 'package:flutter/material.dart';
-
+import 'package:flutter/cupertino.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Home Screen')));
+    return const CupertinoPageScaffold(
+        child: Center(child: Text('Home Screen')));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 import 'infrastructure/presentation/routes/app_router.gr.dart';
 
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    return CupertinoApp.router(
       routerDelegate: _appRouter.delegate(),
       routeInformationParser: _appRouter.defaultRouteParser(),
     );
